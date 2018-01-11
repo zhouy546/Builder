cdcb4a13b3d344525958b3de35be1b4789fa937d
zhouy546 <zhouyunqi546@gmail.com>
2018-01-11 10:14:50 +0800

    add comment
    comment

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -10,8 +10,10 @@ namespace Builder
     {
         static void Main(string[] args)
         {
+            //向左制造引擎，颜色
             AbstractCar car = new Saloon(new StandardEngine(1300));
             VehicleBuilder Builder = new CarBuilder(car);
+            //向右制造汽车框架结构
             VehicleDirector director = new CarDirector();
             Console.WriteLine(director.Build(Builder));
             Console.Read();
